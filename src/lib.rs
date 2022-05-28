40595da493cf9783f2ac1296fca2cd0b4cab14b4
Marcus Holmgren <marcus.holmgren1@gmail.com>
2022-05-28 21:26:13 +0200

    feat: subscriptions endpoint
    Only handles HTTP Form POST of name and email.

--- src/lib.rs
+++ src/lib.rs
@@ -11,11 +11,23 @@ async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
+#[derive(serde::Deserialize)]
+struct FormData {
+    name: String,
+    email: String,
+}
+
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    println!("{:?} contact at: {:?}", _form.name, _form.email);
+    HttpResponse::Ok().finish()
+}
+
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(greet))
             .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
             .route("/{name}", web::get().to(greet))
     })
     .listen(listener)?
