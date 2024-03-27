f44ae00412ef56d3c49c62e35188adb7addd8342
Marcus Holmgren <marcus.holmgren1@gmail.com>
2024-03-27 10:39:54 +0100

    Telemetry (#3)
    * Add simple logging
    
    * Use stuctured logging
    
    * use tracing library to capture log events
    
    * Instrument application with telemetry data.
    
    * Add `tracing-actix-web` to get request_id on all traces

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,24 @@
-use sqlx::PgPool;
+use secrecy::ExposeSecret;
+use sqlx::postgres::PgPool;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
+
     // Panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect(&configuration.database.connection_string())
-        .await
-        .expect("Failed to connect to Postgres.");
-    println!("Connection to Postgres successful.");
+    let connection_pool =
+        PgPool::connect(configuration.database.connection_string().expose_secret())
+            .await
+            .expect("Failed to connect to Postgres.");
+
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address.clone())?;
-    println!("Server listening on http://{}", address);
-    run(listener, connection_pool)?.await
+    run(listener, connection_pool)?.await?;
+    Ok(())
 }
